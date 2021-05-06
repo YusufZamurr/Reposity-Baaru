7eb9b1b3145f4f45f4052a66fc49cbf9858905e1
Yusuf <83707788+YusufZamurr@users.noreply.github.com>
2021-05-06 20:17:19 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Deret_Bilangan_Prima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Masukkan batas atas bilangan prima : ");
+            //inisialisasi
+            bool prima = true;
+            int bilangan = int.Parse(Console.ReadLine());
+
+            if (bilangan >= 2)
+            {
+                //loop hingga batas bilangan tercapai
+                for (int i = 2; i <= bilangan; i++)
+                { //loop mulai dari 2 ke i
+                    for (int j = 2; j < i; j++)
+                    { //bukan bilangan prima jika i habis dibagi j
+                        if ((i % j) == 0)
+                        {
+                            prima = false;
+                            break;
+                        }
+                    }
+                    //jika bilangan == prima
+                    if (prima)
+                        Console.Write(+i + "  ");
+                    prima = true;
+                }
+            }
+            else Console.WriteLine("tidak ada bilangan prima");
+            Console.ReadLine();
+        }
+    }
+}
